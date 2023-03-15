e9382f6e2a017330f28ab76ecd821758cef3aad4
marceloludovino <marcelo.docutec@gmail.com>
2023-03-15 15:02:20 -0300

    add dashboard page to test https

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from 'next'
+
+const Dashboard: NextPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Dashboard page</p>
+    </div>
+  )
+}
+
+export default Dashboard
